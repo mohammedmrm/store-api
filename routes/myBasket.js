0c057b94c7c076c5d25af57d4d5eff2dc46b38b6
mohammedmrm <60153318+mohammedmrm@users.noreply.github.com>
2020-07-29 17:39:37 +0300

    update

--- routes/myBasket.js
+++ routes/myBasket.js
@@ -2,10 +2,11 @@
 const express = require('express');
 const router = express.Router();
 //-----------------Imports-----------------
-const { list, basket, updateBasket} = require('../controllers/myBasket');
+const { list, basket, updateBasket, addToBasket} = require('../controllers/myBasket');
 
 router.get('/myBasket/', list);
 router.get('/myBasket/getBasket', basket);
 router.get('/myBasket/updateBasket', updateBasket);
+router.get('/myBasket/addToBasket', addToBasket);
 
 module.exports = router;
