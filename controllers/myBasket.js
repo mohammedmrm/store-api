260a3691c5f3baf46aa46060139d085d14184d85
mohammedmrm <60153318+mohammedmrm@users.noreply.github.com>
2020-07-29 16:57:52 +0300

    init

--- controllers/myBasket.js
+++ controllers/myBasket.js
@@ -2,7 +2,7 @@ const bcrypt = require('bcrypt');
 const { errorHandler } = require("../helpers/dbErrorHandler");
 var configuration = require("../databaseConfig");
 var con = configuration.connection;
-let success;
+let success=0;
 let data;
 var products = [];
 let access = false;
@@ -44,4 +44,109 @@ exports.list = (req, response) => {
             });
         }
     });
+};
+exports.basket = (req, response) => {
+    try {
+        const username = req.query.username;
+        const password = req.query.password;
+        const basket_id = req.query.basket_id;
+        var id = 0;
+        query = `select * from staff where phone=${username}`;
+        con.query(query, function (err, data) {
+            //console.log('data',data);
+            hash = data[0].password.replace(/^\$2y(.+)$/i, '$2a$1');
+            bcrypt.compare(password, hash, function (err, res) {
+                access = res;
+            });
+            if (access) {
+                id = data[0].id;
+                try {
+                    query = `select * from basket where id=${basket_id} and staff_id=${id}`;
+                    console.log(query);
+                    con.query(query, function (err, data) {
+                        response.json({
+                            code: 200,
+                            success: "1",
+                            data: data,
+                        });
+                    });
+                } catch (err) {
+                    response.json({
+                        code: 200,
+                        success: "0",
+                        data: [{ error: err }],
+                    });
+                }
+            } else {
+                response.json({
+                    code: 300,
+                    success: "0",
+                    message: "incorect username or password"
+                });
+            }
+        });
+    } catch (err) {
+        response.json({
+            code: 200,
+            success: "0",
+            data: [{ error: err }],
+        });
+    }
+};
+exports.updateBasket = (req, response) => {
+    try {
+        const username = req.query.username;
+        const password = req.query.password;
+        const basket_id = req.query.basket_id;
+        const name = req.query.basket_id;
+        const city_id = req.query.city_id;
+        const town_id = req.query.town_id;
+        const address = req.query.address;
+        const note = req.query.note;
+        const phone = req.query.basket_id;
+        var id = 0;
+        query = `select * from staff where phone=${username}`;
+        con.query(query, function (err, data) {
+            //console.log('data',data);
+            hash = data[0].password.replace(/^\$2y(.+)$/i, '$2a$1');
+            bcrypt.compare(password, hash, function (err, res) {
+                access = res;
+            });
+            if (access) {
+                id = data[0].id;
+                try {
+                    query = `update * from basket set
+                            customer_name = ${name},customer_phone=${phone},
+                            address=${address},city_id=${city_id},
+                            town_id=${town_id},note=${note}     
+                            where id=${basket_id} and staff_id=${id}`;
+                    con.query(query, function (err, data) {
+                        response.json({
+                            code: 200,
+                            success: '1',
+                            data: data,
+                        });
+                    });
+                } catch (err) {
+                    response.json({
+                        code: 200,
+                        success: "0",
+                        data: [{ error: err }],
+                    });
+                }
+            } else {
+                response.json({
+                    code: 300,
+                    success: "0",
+                    message: "incorect username or password"
+                });
+            }
+        });
+    } catch (err) {
+        response.json({
+            code: 200,
+            success: "0",
+            data: [{ error: err }],
+        });
+    }
 };
--- routes/myBasket.js
+++ routes/myBasket.js
@@ -2,8 +2,10 @@
 const express = require('express');
 const router = express.Router();
 //-----------------Imports-----------------
-const { list } = require('../controllers/myBasket');
+const { list, basket, updateBasket} = require('../controllers/myBasket');
 
 router.get('/myBasket/', list);
+router.get('/myBasket/getBasket', basket);
+router.get('/myBasket/updateBasket', updateBasket);
 
 module.exports = router;
