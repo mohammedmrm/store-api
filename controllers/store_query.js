bf9ea8b11c86dcecc87c6c1b4647469999a9f39f
mohammedmrm <60153318+mohammedmrm@users.noreply.github.com>
2020-08-10 19:11:06 +0300

    bugs

--- controllers/store_query.js
+++ controllers/store_query.js
@@ -2,22 +2,18 @@ const bcrypt = require('bcrypt');
 const { errorHandler } = require("../helpers/dbErrorHandler");
 var configuration = require("../databaseConfig");
 var con = configuration.connection;
-let success;
-let data;
 let products = [];
 exports.list = (req, response) => {
-  console.log("request arrived for URL", req.url);
-  let limit = req.query.limit ? parseInt(req.query.limit) : 20;
-  let page = ((req.query.page && parseInt(req.query.page) > 0) ? parseInt(req.query.page) : 1);
+  let limit = parseInt(req.query.limit) || 20;
+  let page = parseInt(req.query.page) || 1;
   const search = !req.query.search ? "" : req.query.search;
   const category = !req.query.category ? "" : req.query.category;
-  const list_id = !req.query.list ? "" : req.query.list;
   const username = req.query.username;
   const password = req.query.password;
   let access = false;
   let id = 0;
   try {
-    query = `select product.*,category.title as category_name,
+    sql = `select product.*,category.title as category_name,
             stores.name as store_name,image.img as img
             from product
             left join stores on stores.id = product.store_id
@@ -26,18 +22,15 @@ exports.list = (req, response) => {
             group by product_id) image on image.product_id = product.id
             where product.id <> 0`;
     if (category >= 1) {
-      query += ` and category.id=${category}`;
+      sql += ` and category.id=${category}`;
     }
-    if (list_id >= 1) {
-      query += ` and list_items.list_id=${list_id}`;
-    }    
     if (search != '') {
-      query += ` and (MATCH (product.name) AGAINST ('${search}*' IN BOOLEAN MODE))`;
+      sql += ` and (MATCH (product.name) AGAINST ('${search}*' IN BOOLEAN MODE))`;
     }
     page = (page - 1) * limit;
-    query += ` limit ${page} , ${limit}`;
-    //console.log(query);
-    con.query(query, function (error, data) {
+    sql += ` limit ${page} , ${limit}`;
+    console.log(sql);
+    con.query(sql, function (error, data) {
       let id;
       data.forEach((product) => {
         id = product["id"];
@@ -48,6 +41,7 @@ exports.list = (req, response) => {
               where product.id =${id} group by attribute.id`;
         con.query(sql, function (error, res) {
           let attributes = [];
+
           res.forEach((attr) => {
             // console.log(attr.id);
             sql2 = `select attribute_config.*, attribute.id as attribute_id from attribute
@@ -74,7 +68,7 @@ exports.list = (req, response) => {
       });
       products = [];
     });
-    
+
   } catch (err) {
     response.json({
       code: 200,
@@ -82,19 +76,19 @@ exports.list = (req, response) => {
       data: [{ error: err }],
     });
   }
-  
+
 };
 exports.configrableProduct = (req, response) => {
   let product = req.query.product;
   let config = req.query.options;
   let options = "";
-  let count=0;
+  let count = 0;
   try {
     sql = `select attribute_id from sub_option 
           left join configurable_product on configurable_product.id = sub_option.configurable_product_id
           left join product on product.id = configurable_product.product_id
           where product.id = ${product} GROUP by sub_option.attribute_id`;
-          console.log(sql);
+    console.log(sql);
     con.query(sql, function (error, adj) {
       count = adj.length;
       console.log(count, config.length);
@@ -124,13 +118,13 @@ exports.configrableProduct = (req, response) => {
             data: data,
           });
         });
-      }else{
+      } else {
         response.json({
           code: 200,
           success: "0",
           error: 'select all options',
         });
-       }
+      }
     });
   } catch (err) {
     response.json({
